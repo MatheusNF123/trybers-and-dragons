c5ec24b5df1fe3f6b77dc109c8471e230ce6cfaa
Matheus <matheus_cs.nf@hotmail.com>
2022-10-18 20:33:10 -0300

    implementando a class PVE

---
+++ src/Battle/PVE.ts
@@ -0,0 +1,23 @@
+import Fighter, { SimpleFighter } from '../Fighter';
+// import Monster from '../Monster';
+import Battle from './Battle';
+
+export default class PVE extends Battle {
+  private _player: Fighter;
+  private _monster: SimpleFighter[];
+  constructor(player: Fighter, monster: SimpleFighter[]) {
+    super(player);
+    this._player = player;
+    this._monster = monster;
+  }
+
+  fight(): number {
+    this._monster.forEach((monster) => {
+      while (this._player.lifePoints > 0 && monster.lifePoints > 0) {
+        this._player.attack(monster);
+        monster.attack(this._player);
+      }
+    });
+    return super.fight();
+  }
+}
